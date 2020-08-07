9a0477aa9a5a40d21a611c565bbb71ffc90652d9
Marty <98martychan@gmail.com>
2020-08-07 17:51:12 +0800

    admin interface updated

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -20,4 +20,5 @@ urlpatterns = [
     path('polls/', include('polls.urls')),
     path('todo/', include('todo.urls')),
     path('admin/', admin.site.urls),
+    path('account/', include('django.contrib.auth.urls')),
 ]
