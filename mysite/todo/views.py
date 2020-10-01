927dbdb937ae9b9d8ad0498c9cbedaf2cd6c7de1
v42851 <64484861+v42851@users.noreply.github.com>
2020-10-01 22:53:27 +0530

    Update views.py

--- mysite/todo/views.py
+++ mysite/todo/views.py
@@ -1,3 +1,4 @@
+# Todo and amazing Project
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, HttpResponseRedirect
 from .models import TodoItem, UrgentItem
